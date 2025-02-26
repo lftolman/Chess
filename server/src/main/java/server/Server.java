e3b5475afb4454644760c8c974f23aac04973e4b
lftolman <lftolman@byu.edu>
2025-02-25 20:34:06 -0700

    Initial server implementation

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,32 @@
 package server;
 
+import dataaccess.*;
+import exception.ResponseException;
 import spark.*;
+import service.*;
 
 public class Server {
+    static UserService userService;
+    private GameService gameService;
+    private ClearService clearService;
+    private UserHandler userHandler;
+    private GameHandler gameHandler;
+    private ClearHandler clearHandler;
+
+    public Server(){
+        UserDataAccess userDAO = new UserDAO();
+        GameDataAccess gameDAO = new GameDAO();
+        AuthDataAccess authDAO = new AuthDAO();
+
+        userService = new UserService(userDAO,authDAO);
+        gameService = new GameService(gameDAO,authDAO);
+        clearService = new ClearService(gameDAO,authDAO,userDAO);
+
+        userHandler = new UserHandler(userService);
+        clearHandler = new ClearHandler(clearService);
+        gameHandler = new GameHandler(gameService);
+
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,9 +34,16 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", userHandler::register);
+        Spark.post("/session", userHandler::login);
+        Spark.delete("/session", userHandler::logout);
+        Spark.get("/game", gameHandler::listGames);
+        Spark.post("/game", gameHandler::createGame);
+        Spark.put("/game", gameHandler::joinGame);
+        Spark.put("/db",clearHandler::clearApplication);
 
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        Spark.exception(ResponseException.class,this::exceptionHandler);
+
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -21,5 +52,10 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private void exceptionHandler(ResponseException ex, Request req, Response res) {
+        res.status(ex.StatusCode());
+        res.body(ex.toJson());
     }
 }
