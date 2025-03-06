d3e298fbc741fcf087de9555f2def0674ab6f437
lftolman <lftolman@byu.edu>
2025-03-06 12:29:33 -0700

    SQL AuthDAO (phase 4)

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -1,48 +1,80 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import exception.ResponseException;
 import model.AuthData;
+import model.UserData;
 
 import java.sql.SQLException;
 
-public class MySQLAuthDAO implements AuthDataAccess{
+public class MySQLAuthDAO extends MySQLDataAccess implements AuthDataAccess{
 
-    public MySQLAuthDAO() throws ResponseException {
-        configureDatabase();
+    public MySQLAuthDAO() throws ResponseException, DataAccessException {
+        configureDatabase(createStatements);
     }
 
-    void createAuth(AuthData authData) throws DataAccessException{
-
+    public void createAuth(AuthData authData) throws DataAccessException{
+        try (var conn = DatabaseManager.getConnection()){
+            var statement = "INSERT INTO AuthData (authToken, username) VALUES (?,?)";
+            try (var ps = conn.prepareStatement(statement)){
+                ps.setString(1, authData.authToken());
+                ps.setString(2, authData.username());
+                ps.executeUpdate();
+            }
+        } catch (SQLException e){
+            throw new DataAccessException(e.getMessage());
+        }
     };
 
-    AuthData getAuth(String authToken) throws DataAccessException;
+    public AuthData getAuth(String authToken) throws DataAccessException{
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT authToken, username FROM AuthData WHERE authToken=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return new AuthData(authToken, rs.getString("username"));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    };
 
-    void deleteAuth(String authToken) throws DataAccessException;
+    public void deleteAuth(String authToken) throws DataAccessException{
+        try (var conn = DatabaseManager.getConnection()){
+            var statement = "DELETE FROM AuthData WHERE authToken = ?";
+            try (var ps = conn.prepareStatement(statement)){
+                ps.setString(1, authToken);
+                ps.executeUpdate();
+            }
+        } catch (SQLException e){
+            throw new DataAccessException(e.getMessage());
+        }
+    };
 
-    void deleteAuthData() throws DataAccessException;
+    public void deleteAuthData() throws DataAccessException{
+        try (var conn = DatabaseManager.getConnection()){
+            var statement = "TRUNCATE AuthData";
+            try (var ps = conn.prepareStatement(statement)){
+                ps.executeUpdate();
+            }
+        } catch (SQLException e){
+            throw new DataAccessException(e.getMessage());
+        }
+    };
 
 
     private final String[] createStatements = {
             """
             CREATE TABLE IF NOT EXISTS AuthData (
-              `authToken` varchar(256) DEFAULT NULL,
-              `username` varchar(256) DEFAULT NULL,
-              PRIMARY KEY('authToken'),
+              authToken VARCHAR(256) NOT NULL,
+              username VARCHAR(256) DEFAULT NULL,
+              PRIMARY KEY(authToken)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
 
-
-    private void configureDatabase() throws ResponseException, DataAccessException {
-        DatabaseManager.createDatabase();
-        try (var conn = DatabaseManager.getConnection()) {
-            for (var statement : createStatements) {
-                try (var preparedStatement = conn.prepareStatement(statement)) {
-                    preparedStatement.executeUpdate();
-                }
-            }
-        } catch (SQLException | DataAccessException ex) {
-            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
-        }
-    }
 }
