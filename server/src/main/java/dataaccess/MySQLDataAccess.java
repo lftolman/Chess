96cdbe9c2a6022104257ae0fbb8e2cfb49cccc47
lftolman <lftolman@byu.edu>
2025-03-06 12:27:26 -0700

    Make parent SQL DAO class (phase 4)

---
+++ server/src/main/java/dataaccess/MySQLDataAccess.java
@@ -0,0 +1,20 @@
+package dataaccess;
+
+import exception.ResponseException;
+
+import java.sql.SQLException;
+
+public class MySQLDataAccess {
+    public void configureDatabase(String[] createStatements) throws ResponseException, DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException | DataAccessException ex) {
+            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+}
