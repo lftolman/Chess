06b4feaeb7ccc6c2e6710c47bcd254899fb999ac
lftolman <lftolman@byu.edu>
2025-03-12 12:45:12 -0600

    SQL User Tests (phase 4)

--- server/src/test/java/service/MySQLUserTests.java
+++ server/src/test/java/service/MySQLUserTests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MySQLUserTests {
 
@@ -29,9 +29,61 @@ public class MySQLUserTests {
         try{
             userDAO.deleteUserData();
             UserData user = new UserData("username", "password", "email@email.com");
+            userDAO.insertUser(user);
         } catch (DataAccessException e) {
             fail("Setup failed due to unexpected Exception: " + e.getMessage());
         }
+    }
+
+    @Test
+    @Order(1)
+    void testInsertUserPositive(){
+        try {
+            UserData user = new UserData("username1", "password1", "email1@email.com");
+            userDAO.insertUser(user);
+            UserData givenUser = userDAO.getUser("username1");
+            assertEquals(user.toString(),givenUser.toString());
+        } catch (DataAccessException e) {
+            fail("Setup failed due to unexpected Exception: " + e.getMessage());
+        }
+    }
+
+    @Test
+    @Order(2)
+    void testInsertUserNegative(){
+        UserData user = new UserData("username", "password1", "email1@email.com");
+        Exception e = assertThrows(DataAccessException.class, () ->userDAO.insertUser(user));
+        String message = e.getMessage();
+        assertTrue(message.contains("Duplicate"));
+    }
+
+    @Test
+    @Order(3)
+    void testGetUserPositive(){
+        try{
+            UserData userData = userDAO.getUser("username");
+            UserData expected = new UserData("username", "password", "email@email.com");
+            assertEquals(expected.toString(),userData.toString());
+        } catch (DataAccessException e) {
+            fail("Setup failed due to unexpected Exception: " + e.getMessage());
+        }
+    }
+
+    @Test
+    @Order(4)
+    void testGetUserNegative(){
+        try{
+            UserData userData = userDAO.getUser("usernameWrong");
+            assertNull(userData);
+        } catch (DataAccessException e) {
+            fail("Setup failed due to unexpected Exception: " + e.getMessage());
+        }
+    }
+
+    @Test
+    @Order(5)
+    void testDeleteUserData(){
+        assertDoesNotThrow(()->userDAO.deleteUserData());
     }
 
 }
