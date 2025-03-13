9c156f4cd443ed241f476f70f5f110ace971f06a
lftolman <lftolman@byu.edu>
2025-03-13 10:06:29 -0600

    Code skeleton (phase 5)

---
+++ client/src/main/java/net/ServerFacade.java
@@ -0,0 +1,23 @@
+package net;
+
+public class ServerFacade {
+    public ServerFacade(){
+
+    }
+
+    public String help(){
+        return null;
+    }
+
+    public void quit(){
+
+    }
+
+    public String login(){
+        return null;
+    }
+
+    public String register(){
+        return null;
+    }
+}
