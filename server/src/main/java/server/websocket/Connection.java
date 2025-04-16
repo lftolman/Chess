4a338379904acbedc5bc49fcc59f59d3736b4744
lftolman <lftolman@byu.edu>
2025-04-15 18:03:14 -0600

    websocket connection (phase 6)

---
+++ server/src/main/java/server/websocket/Connection.java
@@ -0,0 +1,21 @@
+package server.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.io.IOException;
+
+public class Connection {
+    public String visitorName;
+    public Session session;
+    public int gameID;
+
+    public Connection(String visitorName, Session session, int gameID) {
+        this.visitorName = visitorName;
+        this.session = session;
+        this.gameID = gameID;
+    }
+
+    public void send(String msg) throws IOException {
+        session.getRemote().sendString(msg);
+    }
+}
